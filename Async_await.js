891aab335713d7043f4b93d9ba1cff6ca5ae0ab6
Sajid Ahamed <109978894+Sajidcodecrack@users.noreply.github.com>
2024-12-27 01:22:03 +0600

    Finally the last concept of this chapter is done

--- Async_await.js
+++ Async_await.js
@@ -3,10 +3,30 @@ function api() {
         setTimeout(() => {
             console.log("Weather Data");
             resolve(200);
+        }, 3000);
+    });
+}
+async function getWeather() {
+    await api();
+}
+function getData(dataid) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Data", dataid);
+            resolve("Success");
+            reject("Failed");
         }, 2000);
     });
 }
-async function getweather(){
-    await api();
+async function getAlldata() {
+    await getData(1234);
+    await getData(5678);
+    await getData(9012);
+    await getData(3456);
+    await getData(7890);
+    await getWeather(4345);
+
 }
-console.log(getweather());
+
+console.log(getWeather());
+console.log(getAlldata());
